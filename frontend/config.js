bb22e9ff3bf0cb64eb921cff46c8c64b5ad945d8
Pinfoxxx <zhirileev.k@gmail.com>
2026-07-18 14:52:35 +0300

    Remove old frontend

--- frontend/config.js
+++
@@ -1,9 +0,0 @@
-window.APP_CONFIG = {
-  mlApiUrl: "http://localhost:8100",
-  // подстрока имени виртуальной камеры, в которую сервер подаёт дипфейк
-  deepfakeCameraLabel: "virtual",
-  enableTestSkip: true,
-  lightSettleMs: 180,
-  lightSampleCount: 4,
-  lightSampleIntervalMs: 70,
-};
